0b575695bd8c5f418e5d2c12849be6ff3cc70db0
Ян <lsvjhm@gmail.com>
2023-09-07 22:37:05 +0300

    W7L4

--- Client/CourceProjectClash/Assets/Scripts/MapInfo.cs
+++ Client/CourceProjectClash/Assets/Scripts/MapInfo.cs
@@ -22,21 +22,40 @@ public class MapInfo : MonoBehaviour
     #endregion
     [SerializeField] private List<Tower> _enemyTowers = new List<Tower>();
     [SerializeField] private List<Tower> _playerTowers = new List<Tower>();
+    [Space]
+    [SerializeField] private List<Unit> _enemyUnits = new List<Unit>();
+    [SerializeField] private List<Unit> _playerUnits = new List<Unit>();
 
+
+    public bool TryGetNearestUnit(in Vector3 currentPosition, out Unit unit, bool enemy, out float distance)
+    {
+        List<Unit> units = enemy ? _enemyUnits : _playerUnits;
+        unit = GetNearest(currentPosition, units, out distance);
+
+        return unit;
+    }
     public Tower GetNearestTower(in Vector3 currentPosition, bool enemy)
     {
         List<Tower> towers = enemy ? _enemyTowers : _playerTowers;
-        Tower nearestTower = towers[0];
-        float distance = Vector3.Distance(currentPosition, towers[0].transform.position);
+        return GetNearest(currentPosition, towers, out float distance);
+    }
 
-        for(int i = 1; i < towers.Count; i++)
+    private T GetNearest<T>(in Vector3 currentPosition, List<T> objects, out float distance) where T : MonoBehaviour
+    {
+        distance = float.MaxValue;
+        if (objects.Count <= 0) return null;
+
+        distance = Vector3.Distance(currentPosition, objects[0].transform.position);
+        T nearest = objects[0];
+
+        for(int i = 1; i < objects.Count; i++)
         {
-            float tempDistance = Vector3.Distance(currentPosition, towers[i].transform.position);
+            float tempDistance = Vector3.Distance(currentPosition, objects[i].transform.position);
             if (tempDistance > distance) continue;
 
-            nearestTower = towers[i];
+            nearest = objects[i];
             distance = tempDistance;
         }
-        return nearestTower;
-    }
+        return nearest;
+    }    
 }
--- Client/CourceProjectClash/Assets/Scripts/UnitParameters.cs
+++ Client/CourceProjectClash/Assets/Scripts/UnitParameters.cs
@@ -4,6 +4,8 @@ public class UnitParameters : MonoBehaviour
 {
     [field: SerializeField] public float speed { get; private set; } = 4f;
     [field: SerializeField] public float modelRadius { get; private set; } = 1f;
+    [field: SerializeField] public float startChaseDistance { get; private set; } = 5f;
+    [field: SerializeField] public float stopChaseDistance { get; private set; } = 7f;
     public float startAttackDistance { get { return modelRadius + _startAttackDistance; } } 
     public float stopAttackDistance { get { return modelRadius + _stopAttackDistance; } }
 
